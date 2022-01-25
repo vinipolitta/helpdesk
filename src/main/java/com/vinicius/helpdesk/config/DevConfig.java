8a441fe40e351a5091f31f5ad89dc41b97ce1a84
vinipolitta <vinicius.politta1@gmail.com>
2022-01-25 17:29:22 -0300

    perfil de dev

--- src/main/java/com/vinicius/helpdesk/config/DevConfig.java
+++ src/main/java/com/vinicius/helpdesk/config/DevConfig.java
@@ -1,7 +1,6 @@
 package com.vinicius.helpdesk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,20 +8,14 @@ import org.springframework.context.annotation.Profile;
 import com.vinicius.helpdesk.services.DBServices;
 
 @Configuration
-@Profile("dev")
-public class TestConfig {
+@Profile("test")
+public class DevConfig {
 	
 	@Autowired
 	private DBServices dbService;
 	
-	@Value("${spring.jpa.hibernate.ddl-auto}")
-	private String value;
-	
 	@Bean
-	public boolean instanciaDB() {
-		if(value.equals("create")) {
-			this.dbService.instaciaDB();
-		}
-		return false;
+	public void instanciaDB() {
+		this.dbService.instaciaDB();
 	}
 }
