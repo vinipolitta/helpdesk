50beb233ae8da9b791573d7b36e11f18aaf86456
vinipolitta <vinicius.politta1@gmail.com>
2022-01-25 08:00:52 -0300

    commit initial

---
+++ src/test/java/com/vinicius/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
