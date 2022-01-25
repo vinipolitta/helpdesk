039cdcbd811abf77d000e95713c6521429a689bc
vinipolitta <vinicius.politta1@gmail.com>
2022-01-25 17:17:50 -0300

    perfil teste criado

---
+++ src/main/java/com/vinicius/helpdesk/config/TestConfig.java
@@ -0,0 +1,21 @@
+package com.vinicius.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.vinicius.helpdesk.services.DBServices;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBServices dbService;
+	
+	@Bean
+	public void instanciaDB() {
+		this.dbService.instaciaDB();
+	}
+}
---
+++ src/main/java/com/vinicius/helpdesk/services/DBServices.java
@@ -0,0 +1,43 @@
+package com.vinicius.helpdesk.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vinicius.helpdesk.domain.Chamado;
+import com.vinicius.helpdesk.domain.Cliente;
+import com.vinicius.helpdesk.domain.Tecnico;
+import com.vinicius.helpdesk.domain.enums.Perfil;
+import com.vinicius.helpdesk.domain.enums.Prioridade;
+import com.vinicius.helpdesk.domain.enums.Status;
+import com.vinicius.helpdesk.repositories.ChamadoRepository;
+import com.vinicius.helpdesk.repositories.ClienteRepository;
+import com.vinicius.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class DBServices {
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+		
+	
+	
+	public void instaciaDB() {
+		Tecnico tec1 = new Tecnico(null, "Vinicius Politta", "19184465042", "vini@teste.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Nayelen kretli", "01797290045", "nayteste@teste.com", "123");
+		cli1.addPerfil(Perfil.CLIENTE);
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+	}
+}
