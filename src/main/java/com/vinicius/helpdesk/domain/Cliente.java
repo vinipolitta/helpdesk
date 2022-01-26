5008c66aa1b3c8418aa24a56233ea560df3c13c9
vinipolitta <vinicius.politta1@gmail.com>
2022-01-26 15:05:15 -0300

    crud para clientes

--- src/main/java/com/vinicius/helpdesk/domain/Cliente.java
+++ src/main/java/com/vinicius/helpdesk/domain/Cliente.java
@@ -1,12 +1,14 @@
 package com.vinicius.helpdesk.domain;
 
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.vinicius.helpdesk.domain.dtos.ClienteDTO;
 import com.vinicius.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -26,6 +28,17 @@ public class Cliente extends Pessoa {
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
---
+++ src/main/java/com/vinicius/helpdesk/services/ClienteService.java
@@ -0,0 +1,82 @@
+package com.vinicius.helpdesk.services;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import java.util.List; 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vinicius.helpdesk.domain.Pessoa;
+import com.vinicius.helpdesk.domain.Cliente;
+import com.vinicius.helpdesk.domain.dtos.ClienteDTO;
+import com.vinicius.helpdesk.repositories.PessoaRepository;
+import com.vinicius.helpdesk.repositories.ClienteRepository;
+import com.vinicius.helpdesk.services.exceptions.DataIntegrityViolationException;
+import com.vinicius.helpdesk.services.exceptions.ObjectNotFoundExeption;
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundExeption("Objeto nao encontrado ID: " + id));
+	}
+
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+
+	public Cliente create(ClienteDTO objDTO) {		
+		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
+		Cliente newObj = new Cliente(objDTO);
+		return repository.save(newObj);
+	}
+	
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+		objDTO.setId(id);
+		Cliente oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Cliente(objDTO);
+		return repository.save(oldObj);		
+	}
+	
+	public void delete(Integer id) {
+		Cliente obj = findById(id);
+		if(obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Cliente possui ordens de serviços e nao pode ser deletado enquanto nao encerra-lo!");
+		}
+		
+		repository.deleteById(id);		
+	}
+
+	private void validaPorCpfEEmail(ClienteDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado do sistema!");
+		}
+		
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("Email já cadastrado do sistema!");
+		}
+	}
+
+	
+
+	
+	
+	
+	
+
+}
