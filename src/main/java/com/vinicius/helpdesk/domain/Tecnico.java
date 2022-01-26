482133fbc06cc593e5ab5077bff3ae60b50919df
vinipolitta <vinicius.politta1@gmail.com>
2022-01-26 10:54:20 -0300

    endpoint created para tecnico

--- src/main/java/com/vinicius/helpdesk/domain/Tecnico.java
+++ src/main/java/com/vinicius/helpdesk/domain/Tecnico.java
@@ -2,11 +2,13 @@ package com.vinicius.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.vinicius.helpdesk.domain.dtos.TecnicoDTO;
 import com.vinicius.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -26,6 +28,17 @@ public class Tecnico extends Pessoa{
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.TECNICO);
+	}
+	
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
